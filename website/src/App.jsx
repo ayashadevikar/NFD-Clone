9bc77701b18002094a9ef0a213715e4f1d7a02b8
ayashadevikar <ayashadevikar8@gmail.com>
2024-01-23 09:49:34 +0530

    first commit

---
+++ website/src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './Navbar';
+import Software from './Software';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+       
+     <Navbar />
+     <Software />
+    </>
+  )
+}
+
+export default App
---
+++ website/src/Software.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Software = () => {
+  return (
+    <>
+    <div>
+
+  
+      <div className='platform'>
+          <h1>NFD platform</h1>
+          <h1>“NFD.gg”</h1>
+          <p className='w-[30rem]'>
+             An Alpha community 2.0 with its own platform for participating, searching, and tracking any crypto related information.
+          </p>
+
+          <div className='buttons'>
+                 <button>Try Platform</button>
+                 <button>How it works?</button>
+          </div>
+      </div>
+
+       <div className='software'>
+
+        </div>
+      </div>
+    </>
+    
+  )
+}
+
+export default Software
